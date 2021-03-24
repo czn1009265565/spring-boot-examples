b3020fb986fbf294a44d4a1b96b00e85e29c1ed8
陈泽南 <chenzenan@jtkjbike.com>
2021-03-24 17:32:54 +0800

    Spring Boot 整合 Docker Compose

---
+++ spring-boot-docker-compose/src/test/java/com/springboot/springbootdockercompose/SpringBootDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootdockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerComposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
