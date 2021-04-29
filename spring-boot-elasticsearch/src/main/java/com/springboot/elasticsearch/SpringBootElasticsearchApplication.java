c0ebac78df5c196a4eb4651ec744a746eebd0239
陈泽南 <chenzenan@jtkjbike.com>
2021-04-29 09:37:01 +0800

    新增dao层扫描

--- spring-boot-elasticsearch/src/main/java/com/springboot/elasticsearch/SpringBootElasticsearchApplication.java
+++ spring-boot-elasticsearch/src/main/java/com/springboot/elasticsearch/SpringBootElasticsearchApplication.java
@@ -2,8 +2,10 @@ package com.springboot.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableElasticsearchRepositories(basePackages = "com.springboot.elasticsearch.dao")
 public class SpringBootElasticsearchApplication {
 
     public static void main(String[] args) {
