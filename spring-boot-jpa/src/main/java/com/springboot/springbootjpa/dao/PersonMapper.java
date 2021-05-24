7085d1cced98433ce864ca09bb265c7e81f25ec7
陈泽南 <chenzenan@jtkjbike.com>
2021-05-24 11:13:00 +0800

    新增自定义查询

--- spring-boot-jpa/src/main/java/com/springboot/springbootjpa/dao/PersonMapper.java
+++ spring-boot-jpa/src/main/java/com/springboot/springbootjpa/dao/PersonMapper.java
@@ -2,10 +2,16 @@ package com.springboot.springbootjpa.dao;
 
 import com.springboot.springbootjpa.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author: zenan
  * @date: 2021/5/24
  */
 public interface PersonMapper extends JpaRepository<Person, Long> {
+
+    Person findByName(String name);
+
+    @Query(value = "select id,name,age from person where name=?1", nativeQuery = true)
+    Person findPersonByName(String name);
 }
