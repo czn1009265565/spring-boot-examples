191f5f9ee5b6b08fcf2763b2887afeb1e2f8f510
陈泽南 <chenzenan@jtkjbike.com>
2021-06-01 14:29:00 +0800

    Spring Boot 整合 xxl-job

---
+++ spring-boot-xxl-job-sample/src/test/java/com/springboot/xxljobsample/SpringBootXxlJobSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.xxljobsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootXxlJobSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
