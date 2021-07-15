b5e9dc63a779516b83659f84d05c15d4c3e14fb6
陈泽南 <chenzenan@jtkjbike.com>
2021-07-15 14:08:44 +0800

    JPA 新增Query注释

--- spring-boot-jpa/src/main/java/com/springboot/springbootjpa/dao/PersonMapper.java
+++ spring-boot-jpa/src/main/java/com/springboot/springbootjpa/dao/PersonMapper.java
@@ -12,6 +12,9 @@ public interface PersonMapper extends JpaRepository<Person, Long> {
 
     Person findByName(String name);
 
+    /**
+     * nativeQuery=true 则查询语句使用原生sql，不加则使用HQL
+     */
     @Query(value = "select id,name,age from person where name=?1", nativeQuery = true)
     Person findPersonByName(String name);
 }
